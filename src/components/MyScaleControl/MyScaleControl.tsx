f27df9539e234f04500897146f9718c1461e504b
greenbackboogie97 <greenbackboogie@tutamail.com>
2022-11-26 17:34:22 +0200

    Added all scale line options and event listeners

--- src/components/MyScaleControl/MyScaleControl.tsx
+++ src/components/MyScaleControl/MyScaleControl.tsx
@@ -1,25 +1,49 @@
 import { useContext, useEffect } from "react";
 import { MapContext } from "../MyMap/MyMap";
-import ScaleLine from "ol/control/ScaleLine";
+import OLScaleLine, { Units } from "ol/control/ScaleLine";
+import { ListenerFunction } from "ol/events";
 
 type Props = {
-  bar?: boolean;
-  text?: boolean;
+  className?: string;
   minWidth?: number;
+  maxWidth?: number;
+  render?: () => unknown;
+  target?: HTMLElement | string;
+  units?: Units;
+  bar?: boolean;
+  steps?: number;
+  text?: boolean;
+  dpi?: number;
+  onChange?: ListenerFunction;
+  onUnitsChange?:ListenerFunction;
+  onError?:ListenerFunction;
+  onPropertyChange?:ListenerFunction;
 };
 
-export default function MyScaleControl(props: Props) {
+export default function ScaleLine(props: Props) {
+  const { onChange, onUnitsChange, onError, onPropertyChange, ...scaleOptions } = props;
   const map = useContext(MapContext);
+
   useEffect(() => {
     if (!map) return;
-    const scaleLine = new ScaleLine(props);
-    const units = scaleLine.getUnits();
+    const scaleLine = new OLScaleLine(scaleOptions);
+
+    onChange && scaleLine.on("change", onChange);
+    onUnitsChange && scaleLine.on("change:units", onUnitsChange);
+    onError && scaleLine.on("error", onError);
+    onPropertyChange && scaleLine.on("propertychange", onPropertyChange);
+
     map?.addControl(scaleLine);
 
     return () => {
-      if (map) map.removeControl(scaleLine);
+      onChange && scaleLine.removeEventListener("change", onChange)
+      onUnitsChange && scaleLine.removeEventListener("change:units", onUnitsChange);
+      onError && scaleLine.removeEventListener("error", onError);
+      onPropertyChange && scaleLine.removeEventListener("propertychange", onPropertyChange);
+
+      map?.removeControl(scaleLine);
     };
-  }, [map, props]);
+  }, [map, onChange, onError, onPropertyChange, onUnitsChange, scaleOptions]);
 
   return null;
 }
